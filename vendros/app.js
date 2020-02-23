06f72ce627b934fff370753297839104c5a1ed00
huseyn <atayevh1@gmail.com>
2020-02-23 11:40:20 +0400

    dowload

---
+++ vendros/app.js
@@ -0,0 +1,30 @@
+// var hbar=document.querySelector(".hidden-bar")
+// var menu=document.querySelector(".fmenu")
+// hbar.onclick=function(){
+//     menu.classList.toggle("active")
+// }
+
+
+// var fli=document.querySelector(".fli")
+// var phmenu=document.querySelector(".phonemenu")
+// fli.onclick=function(){
+//     phmenu.classList.toggle("active")
+// }
+
+// $(window).scroll(function(){
+// var scroll = $(window).scrollTop()
+// $("#mynavbar").css("top","5%")
+// if(scroll < 200){
+//     $('#mynavbar').css('background','transparent')
+// }else{
+//     $('#mynavbar').css('background','linear-gradient(89deg,#a5215e 3%,#ee5586 98%)')
+//     $("mynavbar").css("top","0")
+
+
+
+// }
+// });
+
+// var hbar=document.querySelector(".hidden-bar")
+// var menu=document.querySelector(".fmenu")
+
